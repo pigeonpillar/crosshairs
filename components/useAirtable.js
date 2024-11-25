d63a7326a6312a3509cb5ae9eaf3b6bc39b09024
interpolack <petepolack@gmail.com>
2024-11-25 23:22:55 +0100

    load airtable data

---
+++ components/useAirtable.js
@@ -0,0 +1,59 @@
+import { useState, useEffect, useRef } from 'react';
+import Airtable from 'airtable';
+
+const useAirtable = (baseId, tableName, view) => {
+  const [records, setRecords] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const recordsRef = useRef([]);
+
+  useEffect(() => {
+
+    // localStorage.removeItem(`${baseId}-${tableName}-records`);
+    // return;
+
+    const storedData = localStorage.getItem(`${baseId}-${tableName}-records`);
+
+    if (storedData) {
+      setRecords(JSON.parse(storedData));
+      setLoading(false);
+    } else {
+      const fetchData = async () => {
+        const base = new Airtable({ apiKey: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY }).base(baseId);
+
+        try {
+
+          let out = [];
+
+          base('Open Source').select({
+            maxRecords: 1000,
+            view: 'Grid view'
+          }).eachPage((records, fetchNextPage) => {
+            out = out.concat(records);
+            fetchNextPage();
+          }, err => {
+            if (err) {
+              console.error('Error:', err);
+            }
+            if (!loading) {
+              localStorage.setItem(`${baseId}-${tableName}-records`, JSON.stringify(out));
+            }
+            setRecords(out);
+            setLoading(false);
+          });
+        } catch (err) {
+          setError(err);
+          setLoading(false);
+        }
+      };
+
+      fetchData();
+    }
+
+  }, [baseId, tableName]);
+
+  return { records, loading, error };
+};
+
+export default useAirtable;
